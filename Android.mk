abc908aea869d8f8a30bf142473c1b5d5c38eae4
J0SH1X <aljoshua.hell@gmail.com>
2026-08-20 12:17:57 +0200

    qsd8k: unconditionally include display hals

--- Android.mk
+++ Android.mk
@@ -1,13 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
 display-hals += libmemtrack
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
-display-hals += liblight
-endif
-
 include $(call all-named-subdir-makefiles,$(display-hals))
 
-endif
